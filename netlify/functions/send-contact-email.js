6997c52ae5bf46bedc11450d4dd4907e99c8e851
Shah Bano <whatsmyusername82@gmail.com>
2024-12-13 16:08:44 -0500

    MEOW MEOW

--- netlify/functions/send-contact-email.js
+++ netlify/functions/send-contact-email.js
@@ -1,6 +1,6 @@
 const sgMail = require('@sendgrid/mail');
 
-sgMail.setApiKey(process.env.NETLIFY_EMAILS_PROVIDER_API_KEY);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 
 exports.handler = async function(event, context) {
